54c17c986bfe5675edcce8eec60ed89161d91e3b
angiane <yjf897470@outlook.com>
2022-11-26 22:03:00 +0800

    patched viewontag

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag	    = True;	/* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=16",
 					"WenQuanYi Micro Hei:size=16:type=Regular:antialias=true:autohint=true",
 					"Symbols Nerd Font:pixelsize=20:type=2048-em:antialias=true:autohint=true"};
--- dwm.c
+++ dwm.c
@@ -1831,6 +1831,8 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+		if(viewontag && ((arg->ui & TAGMASK) != TAGMASK))
+			view(arg);
 	}
 }
 
