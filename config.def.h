d9d817d4afc75a4ab5228d464b3feb89c4f229bc
angiane <yjf897470@outlook.com>
2022-11-30 10:17:42 +0800

    change dmenu to rofi

--- config.def.h
+++ config.def.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "yang", "-modi", "yang:/home/yang/Arch/dwm/scripts/rofi.sh", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL};
---
+++ scripts/rofi.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+case "$*" in
+	"proxy")
+		nohup clash >/dev/null 2>&1 &
+		;;
+	"unproxy")
+		nohup pkill clash >/dev/null 2>&1 &
+		;;
+	"chrome")
+		nohup google-chrome-stable -force-device-scale-factor=1.2 >/dev/null 2>&1 &
+		;;
+	"volume down")
+		sh /home/yang/Arch/dwm/scripts/volume_down.sh >/dev/null 2>&1
+		;;
+	"volume up")
+		sh /home/yang/Arch/dwm/scripts/volume_up.sh >/dev/null 2>&1
+		;;
+	"volume toggle")
+		sh /home/yang/Arch/dwm/scripts/volume_mute_or_unmute.sh >/dev/null 2>&1
+		;;
+	"backlight down")
+		sh /home/yang/Arch/dwm/scripts/backlight_down.sh >/dev/null 2>&1
+		;;
+	"backlight up")
+		sh /home/yang/Arch/dwm/scripts/backlight_up.sh >/dev/null 2>&1
+		;;
+	"microphone toggle")
+		sh /home/yang/Arch/dwm/scripts/microphone_cap_or_nocap.sh >/dev/null 2>&1
+		;;
+	"microphone down")
+		sh /home/yang/Arch/dwm/scripts/microphone_down.sh >/dev/null 2>&1
+		;;
+	"microphone up")
+		sh /home/yang/Arch/dwm/scripts/microphone_up.sh >/dev/null 2>&1
+		;;
+	*)
+		echo "proxy"
+		echo "chrome"
+		echo "unproxy"
+		echo "volume down"
+		echo "volume up"
+		echo "volume toggle"
+		echo "backlight down"
+		echo "backlight up"
+		echo "microphone toggle"
+		echo "microphone down"
+		echo "microphone up"
+esac
